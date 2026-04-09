d0ac6b447d3b86adaf6f74c72e165d2b8c9aa3e6
Jose Munoz-Suastegui <jmunozsuastegui@ggc.edu>
2026-04-08 21:29:45 -0400

    add alternate morning check

--- 04-life.sh
+++ 04-life.sh
@@ -14,3 +14,10 @@ fi
 
 # exercise: write a script that prints whether it is 
 # morning or not
+hour=$(date +%H)
+
+if [ "$hour" -lt 12 ]; then
+  echo "Good morning"
+else
+  echo "Good afternoon or evening"
+fi
